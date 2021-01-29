45587328f9a7bd62512222cb4a2b5be7c247c194
aksharma-7 <52443790+aksharma-7@users.noreply.github.com>
2021-01-29 18:34:44 +0530

    Added Header Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="app">
-      <h1>Tinder</h1>
-
-      {/* Header */}
+      <Header />
       {/* Tinder Card */}
       {/* SwipeButtons */}
     </div>
