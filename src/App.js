379129a19244595a7751a6a18c6ab2339c2ac88d
aksharma-7 <52443790+aksharma-7@users.noreply.github.com>
2021-03-26 21:57:16 +0530

    added swipe buttons and resolved card bug

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import Header from './components/Header';
 import TinderCards from './components/TinderCards';
+import SwipeButtons from './components/SwipeButtons';
 
 function App() {
   return (
-    <div className="app">
+    <div className='app'>
       <Header />
       <TinderCards />
-      {/* SwipeButtons */}
+      <SwipeButtons />
     </div>
   );
 }
