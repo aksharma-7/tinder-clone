d6d1aa915c0bcb32b31c0b1212054cff62050e1f
aksharma-7 <52443790+aksharma-7@users.noreply.github.com>
2021-03-27 19:37:05 +0530

    added get and post api

--- tinder-backend/server.js
+++ tinder-backend/server.js
@@ -1,16 +1,52 @@
 import express from 'express';
-import mongodb from 'mongoose';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import Cors from 'cors';
+
+import Cards from './dbCards.js';
+
+dotenv.config();
 
 // App Config
 const app = express();
 const port = process.env.PORT || 8000;
+const connection_url = process.env.CONNECTION_URL;
 
 // Middlewares
+app.use(express.json());
+app.use(Cors());
 
 // DB Config
+mongoose.connect(connection_url, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
 
 // API Endpoints
 app.get('/', (req, res) => res.status(200).send('Hello World'));
+
+app.post('/tinder/cards', (req, res) => {
+  const dbCard = req.body;
+
+  Cards.create(dbCard, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(data);
+    }
+  });
+});
+
+app.get('/tinder/cards', (req, res) => {
+  Cards.find((err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  });
+});
 
 // Listener
 app.listen(port, () => console.log(`Listening on port: ${port}`));
